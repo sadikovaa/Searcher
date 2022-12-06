d329c5203c03c9920d5d394746b44a3dad903929
GitHub Action <action@github.com>
2022-12-06 18:44:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/sadikovaa/searcher/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sadikovaa/searcher/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sadikovaa.searcher.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sadikovaa.searcher.MyBundle
 
 class MyApplicationService {
 
